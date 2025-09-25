ad44bc5d06c966d32b791ba714510e0fd266d79c
Your name here <elia.orsini@hotmail.com>
2025-09-25 14:05:30 +0100

    chore: update dependencies

--- viewer/src/Three.jsx
+++ viewer/src/Three.jsx
@@ -1,102 +1,126 @@
-import './App.css';
-import { useEffect, useState } from 'react'
-import { useThree } from '@react-three/fiber'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { useThree } from "@react-three/fiber";
 import Scene from "./Scene";
-import { OrbitControls, Environment} from '@react-three/drei';
-import { useControls, button } from 'leva'
-import * as THREE from 'three'
+import { OrbitControls, Environment } from "@react-three/drei";
+import { useControls, button } from "leva";
+import * as THREE from "three";
 
 function Three() {
+  const { scene, camera } = useThree();
 
-    const {scene, camera} = useThree()
-    const [blenderCamera, setBlenderCamera] = useState(false)
+  camera.position.y = 4;
 
-    camera.position.y = 4
+  let localColor;
+  if (localStorage.getItem("bgColor")) {
+    localColor = localStorage.getItem("bgColor");
+  } else {
+    localColor = "#272727";
+  }
+  const bgColor = useControls({ bgColor: localColor });
 
-    let localColor
-    if (localStorage.getItem('bgColor')) {
-        localColor = localStorage.getItem('bgColor')
-    } else {
-        localColor = '#272727'
-    }
-    const bgColor = useControls({ bgColor: localColor })
+  let localGrid;
+  if (localStorage.getItem("grid")) {
+    localGrid = localStorage.getItem("grid") === "true";
+  } else {
+    localGrid = true;
+  }
+  const grid = useControls({ floor: localGrid });
 
-    let localGrid
-    if (localStorage.getItem('grid')) {
-        localGrid = localStorage.getItem('grid') === 'true'
-    } else {
-        localGrid = true
-    }
-    const grid = useControls({ floor: localGrid })
+  let initAutoRotate;
+  if (localStorage.getItem("autorotate")) {
+    initAutoRotate = localStorage.getItem("autorotate") === "true";
+  } else {
+    initAutoRotate = false;
+  }
+  const autoRotate = useControls({ autoRotate: initAutoRotate });
 
-    let initAutoRotate
-    if (localStorage.getItem('autorotate')) {
-        initAutoRotate = localStorage.getItem('autorotate') === 'true'
-    } else {
-        initAutoRotate = false
-    }
-    const autoRotate = useControls({ autoRotate: initAutoRotate })
+  let localAxes;
+  if (localStorage.getItem("axes")) {
+    localAxes = localStorage.getItem("axes") === "true";
+  } else {
+    localAxes = true;
+  }
+  const axes = useControls({ axesHelper: localAxes });
 
-    let localAxes
-    if (localStorage.getItem('axes')) {
-        localAxes = localStorage.getItem('axes') === 'true'
-    } else {
-        localAxes = true
-    }
-    const axes = useControls({ axesHelper: localAxes })
+  const bg = useControls({
+    bg: {
+      options: [
+        "color",
+        "dawn",
+        "night",
+        "sunset",
+        "warehouse",
+        "forest",
+        "apartment",
+        "studio",
+        "city",
+        "park",
+        "lobby",
+      ],
+    },
+  });
 
-    useControls({
-        blenderView: button(() => {setBlenderCamera(true)})
-    })
+  // cache values
+  useEffect(() => {
+    localStorage.setItem("bgColor", bgColor.bgColor);
+    localStorage.setItem("grid", grid.floor);
+    localStorage.setItem("axes", axes.axesHelper);
+    localStorage.setItem("autorotate", autoRotate.autoRotate);
+  }, [bgColor, grid, axes, autoRotate]);
 
-    const bg = useControls({
-        bg: {options: ['color',  'dawn', 'night', 'sunset', 'warehouse', 'forest', 'apartment', 'studio', 'city', 'park', 'lobby']}
-    })
+  // adjust light intensity
+  useEffect(() => {
+    scene.children.map((child) => {
+      if (child.type === "Group") {
+        child.children.map((subchild) => {
+          if (subchild.type === "Group")
+            subchild.children.map((subsubchild) => {
+              if (
+                subsubchild.type === "PointLight" ||
+                subsubchild.type === "SpotLight"
+              )
+                subsubchild.intensity = subsubchild.intensity / 1200;
+              return null;
+            });
+          return null;
+        });
+      }
+      return null;
+    });
+  }, [scene.children]);
 
-    // cache values
-    useEffect(()=>{
-        localStorage.setItem('bgColor', bgColor.bgColor)
-        localStorage.setItem('grid', grid.floor)
-        localStorage.setItem('axes', axes.axesHelper)
-        localStorage.setItem('autorotate', autoRotate.autoRotate)
-    }, [bgColor, grid, axes, autoRotate])
+  return (
+    <>
+      <ambientLight intensity={0.1} />
+      {bg.bg !== "color" ? (
+        <Environment preset={bg.bg} background />
+      ) : (
+        <color attach="background" args={[bgColor.bgColor]} />
+      )}
 
-    // adjust light intensity
-    useEffect(()=>{
-        scene.children.map(child => {
-            if (child.type === "Group") {
-                child.children.map(subchild => {
-                    if (subchild.type === 'Group')
-                        subchild.children.map(subsubchild => {
-                            if (subsubchild.type === 'PointLight' || subsubchild.type === 'SpotLight') subsubchild.intensity = subsubchild.intensity/1200
-                            return null
-                        })
-                    return null
-                })
-            }
-            return null
-        })
-    }, [scene.children])
+      <Scene />
 
-    return (
-        <>
-            <ambientLight intensity={0.1} />
-            {bg.bg !== 'color' ? (<Environment preset={bg.bg} background/>) : (<color attach="background" args={[bgColor.bgColor]} />)}
-            
-            <Scene blenderCamera={blenderCamera}/>
+      {axes.axesHelper && <primitive object={new THREE.AxesHelper(100)} />}
 
-            {axes.axesHelper ? (<primitive object={new THREE.AxesHelper(60)} />) : (<></>)}
-            <mesh rotation={[Math.PI/2,Math.PI,Math.PI]} position={[0,0,0]}>
-                <planeBufferGeometry args={[120,120,100,100]}/>
-                <meshBasicMaterial color="#f3f3f3" wireframe opacity={grid.floor ? (0.07) : (0)} transparent/>
-            </mesh>
-            { blenderCamera ? (
-                <></>
-            ) : (
-                <OrbitControls minDistance={10} makeDefault autoRotate={autoRotate.autoRotate} autoRotateSpeed={1.2}/>
-            )}
-        </>
-    )
+      <mesh rotation={[Math.PI / 2, Math.PI, Math.PI]} position={[0, 0, 0]}>
+        <planeGeometry args={[350, 350, 100, 100]} />
+        <meshBasicMaterial
+          color="#f3f3f3"
+          wireframe
+          opacity={grid.floor ? 0.07 : 0}
+          transparent
+        />
+      </mesh>
+
+      <OrbitControls
+        minDistance={10}
+        makeDefault
+        autoRotate={autoRotate.autoRotate}
+        autoRotateSpeed={1.2}
+      />
+    </>
+  );
 }
 
 export default Three;
